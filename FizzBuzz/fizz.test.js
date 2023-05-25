4fa0e1081aa590066dafdafbd2e6ceac2fac5c2d
Alexmaingi <alexmaingi077@gmail.com>
2023-05-25 13:46:44 +0300

    changes

--- FizzBuzz/fizz.test.js
+++ FizzBuzz/fizz.test.js
@@ -1,8 +1,15 @@
 const fizzBuzz = require("./fizz.js");
 
+test("should log buzz when input is divisible by 3", () => {
+  const consoleSpy = jest.spyOn(console, "log");
+  fizzBuzz(3);
+  expect(consoleSpy).toHaveBeenCalledWith("fizz");
+  consoleSpy.mockRestore();
+});
+
 test("should log fizz when input is divisible by 5", () => {
   const consoleSpy = jest.spyOn(console, "log");
-  fizzBuzz(10);
+  fizzBuzz(5);
   expect(consoleSpy).toHaveBeenCalledWith("buzz");
   consoleSpy.mockRestore();
 });
@@ -11,12 +18,5 @@ test("should log fizzbuzz when input is divisible by 3 and 5", () => {
   const consoleSpy = jest.spyOn(console, "log");
   fizzBuzz(15);
   expect(consoleSpy).toHaveBeenCalledWith("fizzbuzz");
-  consoleSpy.mockRestore();
-});
-
-test("should log buzz when input is divisible by 3", () => {
-  const consoleSpy = jest.spyOn(console, "log");
-  fizzBuzz(9);
-  expect(consoleSpy).toHaveBeenCalledWith("fizz");
   consoleSpy.mockRestore();
 });
